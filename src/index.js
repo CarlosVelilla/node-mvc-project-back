0e925e0f198525aec7c93379d4da978eede0e0e8
tamtran <tamtran2885@gmail.com>
2022-01-10 14:21:13 +0100

    ...

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-require('dotenv').config()
-
 const app = require("./server");
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server listening on http:localhost:${PORT}`);
