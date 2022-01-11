e80fc796521bfc3eca1a1a00d6c7ebb324043ab6
tamtran <tamtran2885@gmail.com>
2022-01-11 13:12:04 +0100

    fix input description

--- src/models/Book.js
+++ src/models/Book.js
@@ -4,22 +4,22 @@ const BookSchema = new mongoose.Schema(
   {
     title: {
       type: String,
-      required: [true, "The last name is required"],
+      required: [true, "The title is required"],
       trim: true,
     },
     description: {
       type: String,
-      required: [true, "The last name is required"],
+      required: [true, "The description is required"],
       trim: true,
     },
     price: {
       type: Number,
-      required: [true, "The last name is required"],
+      required: [true, "The price is required"],
       trim: true,
     },
     in_stock: {
       type: Number,
-      required: [true, "The last name is required"],
+      required: [true, "The number is required"],
       trim: true,
     },
   },
